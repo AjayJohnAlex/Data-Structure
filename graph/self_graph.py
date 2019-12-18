7824214640bee983612a3e026bbc304ee0d94fce
AjayJohnAlex <ajay.alex96@gmail.com>
2019-12-18 12:49:52 +0530

    Finding shortest path code added

--- graph/self_graph.py
+++ graph/self_graph.py
@@ -20,8 +20,54 @@ def generate_graph(graph):
 
     return edges
 
+# finding the path between 2 vertices
 
-addEdge(graph, 'a', 'b')
-addEdge(graph, 'b', 'c')
 
-print(generate_graph(graph))
+def findPath(graph, start, end, path=[]):
+
+    path = path + [start]
+    if start == end:
+        return path
+
+    for vertice in graph[start]:
+        if vertice not in path:
+            newpath = findPath(graph, vertice, end, path)
+            # if newpath:
+            return newpath
+            # return None
+
+
+def shortest_path(graph, start, end, path=[]):
+
+    path = path + [start]
+    if start == end:
+        return path
+    shortestPath = None
+    for vertices in graph[start]:
+        if vertices not in path:
+            print('path', path)
+            newpath = shortest_path(graph, vertices, end, path)
+            print('newpath', newpath)
+            # if newpath:
+            #     if not shortestPath or len(newpath) < len(shortestPath):
+            #         shortestPath = newpath
+
+    return newpath
+
+
+addEdge(graph, 'a', 'c')
+addEdge(graph, 'b', 'd')
+addEdge(graph, 'a', 'd')
+# addEdge(graph, 'c', 'e')
+addEdge(graph, 'd', 'a')
+addEdge(graph, 'd', 'f')
+# addEdge(graph, 'e', 'b')
+# addEdge(graph, 'e', 'c')
+addEdge(graph, 'c', 'e')
+addEdge(graph, 'e', 'f')
+
+# print(generate_graph(graph))
+# print(findPath(graph, 'a', 'f'))
+print(graph.items())
+print(shortest_path(graph, 'a', 'f'))
+# print(find_shortest_path(graph, 'a', 'f'))
